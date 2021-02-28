dcbdcac5bb7ffebadd7a58b59fd65a490f21167e
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2021-02-28 18:26:54 -0300

    UpdateRefatoracao

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-### Treinamento Digital Innovation One - Projeto Jogo Da Cobrinha.
-- O projeto publicado é referente ao treinamento do Curso BootCamp-HTML WebDeveloper da Digital Innovation One (https://digitalinnovation.one).
+### Projeto: Jogo Da Cobrinha.
 
 - Foi publicado uma pagina onde consta o Jogo Da Cobrinha.  Foi utilizado as tecnologias voltado ao FrontEnd + HTML5 + CSS3 + JavaScript + Canvas . Aplicado conceitos como: Criação de Containers. Responsividade. Contador de pontuação do Jogo.  Contagem do Tempo do Jogo. Reiniciar tempo e pontuação caso perca o Jogo. Aplicação de cores dentro do canvas (rgba).
 
 - Site publicado no link: https://camila-github.github.io/projeto-dio-canvas-js-jogo-cobrinha/
+
+- Atualização - Data 28-02-2021 - 
+Realizado refatoração. Aplicado conceitos de Codigo Limpo. Diminuido tamanho das funções, com uma a duas funcionalidades.
+
+- O projeto publicado é referente ao treinamento dos Cursos da Digital Innovation One (https://digitalinnovation.one).
--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 var cobraDirecaoX = cobraDirecaoY = 10;
 var comidaDirecaoX = comidaDirecaoY = 15;
 var telaDirecaoX = telaDirecaoY = 0;
-grid = 20;
-tamanhoCauda = [];
-tamanhoPadraoCauda = 2;
-pontuacao = 0;
+var grid = 20;
+var tamanhoCauda = [];
+var tamanhoPadraoCauda = 2;
+var pontuacao = 0;
 
 var tempo_segundos = new Number();
 var tempo_segundos = 1;
 var zerarTempo = 0;
 
-
 window.onload = function() {
-    quadroCanvas = document.getElementById("quadroCanvas");
+    var quadroCanvas = document.getElementById("quadroCanvas");
     ctx = quadroCanvas.getContext("2d");
     document.addEventListener("keydown", teclaPressionada);
     setInterval(renderizaJogo, 100);
@@ -22,7 +21,6 @@ window.onload = function() {
 
 // telaDirecaoX e telaDirecaoY aplica a direção que a cobra irá percorrer
 function teclaPressionada(event) {
-
     console.log(event.keyCode);
 
     switch (event.keyCode) { //verifica qual a tecla foi pressionada
@@ -46,52 +44,32 @@ function teclaPressionada(event) {
 }
 
 
+function corFundoTelaQuadroCanvas() {
+ //aplica cor em gradient no fundo da tela do quadroCanvas
+ var corQuadroCanvas = ctx.createLinearGradient(0, 100, 700, 200);
+ corQuadroCanvas.addColorStop(0, "#21610B");
+ corQuadroCanvas.addColorStop(1, "white");
+ ctx.fillStyle = corQuadroCanvas;
+ ctx.fillRect(0, 0, quadroCanvas.width, quadroCanvas.height);
 
-//Renderiza a posição da cobra na tela
-function renderizaJogo() {
+}
 
-    cobraDirecaoX += telaDirecaoX; // A posicao da cobra (cobraDirecaoX), recebe a posição da tela onde a cobra esta (telaDirecaoX), isso tambem somando com ela mesma (cobraDirecaoX = cobraDirecaoX + telaDirecaoX)
-    cobraDirecaoY += telaDirecaoY;
+function aumentaTamanhoCauda() {
+      //Se a cobra comer, aumenta o tamanho da cauda.
+      ctx.fillStyle = "#0B1907";
+      for (let i = 0; i < tamanhoCauda.length; i++) {
+          ctx.fillRect(tamanhoCauda[i].x * grid, tamanhoCauda[i].y * grid, grid - 1, grid - 1);
+          if (tamanhoCauda[i].x == cobraDirecaoX && tamanhoCauda[i].y == cobraDirecaoY) {
+              tamanhoPadraoCauda = 2; // a cobra voltará para o seu tamanho incial, caso a cobra bata nela mesma.
+              tempo_segundos = 0;
+          }  
+      }
+      tamanhoCauda.push({ x: cobraDirecaoX, y: cobraDirecaoY });
+      pontuacao = tamanhoCauda.length;
+      while (tamanhoCauda.length > tamanhoPadraoCauda) tamanhoCauda.shift();
+}
 
-    console.log(cobraDirecaoX);
-    if (cobraDirecaoX < 0) {
-        cobraDirecaoX = grid - 1;
-    }
-    if (cobraDirecaoX > grid - 1) {
-        cobraDirecaoX = 0;
-    }
-    if (cobraDirecaoY < 0) {
-        cobraDirecaoY = grid - 1;
-    }
-    if (cobraDirecaoY > grid - 1) {
-        cobraDirecaoY = 0;
-    }
-
-    //aplica cor em gradient no fundo da tela do quadroCanvas
-    var corQuadroCanvas = ctx.createLinearGradient(0, 100, 700, 200);
-    corQuadroCanvas.addColorStop(0, "#21610B");
-    corQuadroCanvas.addColorStop(1, "white");
-    ctx.fillStyle = corQuadroCanvas;
-    ctx.fillRect(0, 0, quadroCanvas.width, quadroCanvas.height);
-
-    //Se a cobra comer, aumenta o tamanho da cauda 
-    ctx.fillStyle = "#0B1907";
-    for (var i = 0; i < tamanhoCauda.length; i++) {
-        ctx.fillRect(tamanhoCauda[i].x * grid, tamanhoCauda[i].y * grid, grid - 1, grid - 1);
-        if (tamanhoCauda[i].x == cobraDirecaoX && tamanhoCauda[i].y == cobraDirecaoY) {
-            tamanhoPadraoCauda = 2; // a cobra voltará para o seu tamanho incial, caso a cobra bata nela mesma.
-            tempo_segundos = 0;
-        }
-
-    }
-    tamanhoCauda.push({ x: cobraDirecaoX, y: cobraDirecaoY });
-    pontuacao = tamanhoCauda.length;
-    while (tamanhoCauda.length > tamanhoPadraoCauda) {
-        tamanhoCauda.shift();
-
-    }
-
-
+function reposicionaComidaNaTela() {
     //Se a cobra comer, reposiciona a comida na tela
     ctx.fillStyle = "red";
     ctx.fillRect(comidaDirecaoX * grid, comidaDirecaoY * grid, grid - 1, grid - 1);
@@ -99,40 +77,57 @@ function renderizaJogo() {
         tamanhoPadraoCauda++;
         comidaDirecaoX = Math.floor(Math.random() * grid);
         comidaDirecaoY = Math.floor(Math.random() * grid);
-
     }
+}
 
-
-    //Será mostrado a pontuação
+function mostraPontuacao() {
+    //Mostra a pontuação
     pontuacao = pontuacao - 3;
     document.getElementById("pontuacao").innerHTML = pontuacao;
+}
+
+//Renderiza a posição da cobra na tela
+function renderizaJogo() {
+    cobraDirecaoX += telaDirecaoX; // A posicao da cobra (cobraDirecaoX), recebe a posição da tela onde a cobra esta (telaDirecaoX), isso tambem somando com ela mesma (cobraDirecaoX = cobraDirecaoX + telaDirecaoX)
+    cobraDirecaoY += telaDirecaoY;
+
+    console.log(cobraDirecaoX);
+    if (cobraDirecaoX < 0) cobraDirecaoX = grid - 1;
+
+    if (cobraDirecaoX > grid - 1) cobraDirecaoX = 0;
+    
+    if (cobraDirecaoY < 0) cobraDirecaoY = grid - 1;
+    
+    if (cobraDirecaoY > grid - 1) cobraDirecaoY = 0;    
+    
+    corFundoTelaQuadroCanvas(); //aplica cor em gradient no fundo da tela do quadroCanvas
+    
+    aumentaTamanhoCauda();  //Se a cobra comer, aumenta o tamanho da cauda     
+   
+    reposicionaComidaNaTela(); //Se a cobra comer, reposiciona a comida na tela    
+    
+    mostraPontuacao(); //Mostra a pontuação
 }
 
 
 //Mostra o tempo do jogo com duas casas decimais (hora, minuto,segundo)
 function contadorTempo() {
-    if (tempo_segundos > zerarTempo) {
+    if (tempo_segundos++ > zerarTempo) {
         formatacaoTempo(tempo_segundos);
-        tempo_segundos++;
-
     } else {
         document.getElementById("time").innerHTML = "00:00:00";
         tempo_segundos++;
     }
 }
 
-
 function formatacaoTempo(s) {
     function duas_casas(numero) {
-        if (numero <= 9) {
-            numero = "0" + numero;
-        }
-        return numero;
+       if (numero <= 9) numero = "0" + numero;
+       return numero;
     }
-    var hora = duas_casas(Math.trunc(s / 3600));
-    var minuto = duas_casas(Math.trunc((s % 3600) / 60));
-    var segundo = duas_casas((s % 3600) % 60);
-    var formatado = hora + ":" + minuto + ":" + segundo;
-    document.getElementById("time").innerHTML = formatado;
-    return formatado;
+    let hora = duas_casas(Math.trunc(s / 3600));
+    let minuto = duas_casas(Math.trunc((s % 3600) / 60));
+    let segundo = duas_casas((s % 3600) % 60);
+    let formatado = hora + ":" + minuto + ":" + segundo;
+    return document.getElementById("time").innerHTML = formatado;   
 }
